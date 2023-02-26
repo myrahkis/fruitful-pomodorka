70172f5b9e9f3c3011529690cfa5417664900d5e
Mira <kruzinka@yandex.ru>
2023-02-26 22:33:45 +0300

    added view, url, adjusted settings

--- fruitful_pomodorka/urls.py
+++ fruitful_pomodorka/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from fruitful_pomodorka import settings
+from pomodoro import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page_view, name='home'),
-]
+    path('', views.index_view, name='home'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
