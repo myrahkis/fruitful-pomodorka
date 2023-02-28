9ee83558ad57073ad08d1c0febf236cd28bd1043
Mira <kruzinka@yandex.ru>
2023-02-28 22:15:14 +0300

    api and schema

--- fruitful_pomodorka/urls.py
+++ fruitful_pomodorka/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 
 from fruitful_pomodorka import settings
 from pomodoro import views
+from pomodoro.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_view, name='home'),
+    path('api/', api.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pomodoro/models.py
+++ pomodoro/models.py
@@ -5,9 +5,6 @@ from django.utils import timezone
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
 
-    # class Meta:
-    #     ordering = ['-created']
-
     def __str__(self):
         return self.title
 
@@ -22,9 +19,6 @@ class ToDoItem(models.Model):
     description = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(default=one_week_hence())
-
-    # class Meta:
-    #     ordering = ['-created']
 
     def __str__(self):
         return f"{self.title}: due {self.due_date}"
