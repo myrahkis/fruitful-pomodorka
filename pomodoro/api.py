f741f065a77de66381d6d7554168981c9f3ee5ae
Mira <kruzinka@yandex.ru>
2023-03-02 13:05:34 +0300

    api

--- pomodoro/api.py
+++ pomodoro/api.py
@@ -1,8 +1,31 @@
 from ninja import NinjaAPI
-from schema import *
-from models import *
+from .schema import *
+from .models import *
+from django.shortcuts import get_object_or_404
 
 api = NinjaAPI()
 
-@api.post("/")
+
+@api.post("/todo", tags=['Todo'])
+def add_todo(request, payload: AddTodo):
+    todo = Todo.objects.create(**payload.dict())
+    return {"id": todo.id}
+
+
+@api.get("/todo", tags=['Todo'], response=DeleteTodo)
+def get_todo(request, todo_id: int):
+    todo = get_object_or_404(Todo, id=todo_id)
+    return todo
+
+
+@api.patch("/todo/{todo_id}", tags=['Todo'])
+def update_todo(request, todo_id: int, payload: UpdateTodo):
+    todo = get_object_or_404(Todo, id=todo_id)
+    for atr, value in payload.dict().items():
+        setattr(todo, atr, value)
+    todo.save()
+    return {"Success": True}
+
+
+
 
